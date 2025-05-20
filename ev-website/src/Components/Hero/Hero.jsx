2e9f082169418f755c8ccd74b5a75b56b764a8f5
Expo-67 <145459052+Expo-67@users.noreply.github.com>
2025-05-20 19:56:43 +0300

    background setup

---
+++ ev-website/src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <p> Heo here</p>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ ev-website/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p> Navbar goes here</p>
+    </div>
+  );
+};
+
+export default Navbar;
